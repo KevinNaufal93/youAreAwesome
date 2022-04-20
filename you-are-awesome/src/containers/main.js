2f268009fc1a895370d6a3042946e6308adb8662
MorningToast21 <naufal.kevin7@outlook.com>
2022-04-20 11:17:45 +0700

    progress 2
    * finished for now

--- you-are-awesome/src/containers/main.js
+++ you-are-awesome/src/containers/main.js
@@ -2,30 +2,58 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import "./main.css";
 import { ActionButton } from '../components/actionButton';
-import profPic from "../assets/img/testKevin.jpg"
+import profPic from "../assets/img/2.png"
 
 export const MainPage = () => {
-    useEffect(() => {
-        imageClicked ? 
-        console.log(imageClicked) : 
-        console.log("False no")
-      });
+
     const [imageClicked, setImageClicked] = useState(false)
+    const [buttonActivated, setButtonActivated] = useState("none")
+    const MINUTE_MS = 60000;
+
+    const clickHandler = () => {
+        console.log("entering click Handler")
+        setButtonActivated("hidden")
+        setImageClicked(true)
+        console.log(`imageClicked is ${imageClicked} and buttonActivated is ${buttonActivated}`)
+    }
+
+    // useEffect(() => {
+    //   const interval = setInterval(() => {
+    //     console.log('Logs every minute');
+    //     setImageClicked(false)
+    //   }, MINUTE_MS);
+    //   return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    // }, [])
+
     return (
-        <div>
-            <div className="header">
+        <div className="h-screen">
+        <div
+        className="flex flex-col items-center h-screen">
+            {imageClicked ? 
+                <div className="subheader">
+                <span className='font-semibold'>You</span> have the cutest eyes 
+                </div>
+                :
+                <div className="header">
                 Button Button on the wall who's the most beautiful of them all?
+                </div>
+                }
+            <div
+                onClick={()=>clickHandler()}>
+                    {imageClicked ?
+                        <img 
+                            src={profPic}
+                            className="max-w-xs max-h-fit" 
+                        /> : 
+                        <ActionButton
+                        style={{visibility: buttonActivated}}
+                        className="flex-none "
+                    />
+                    } 
+
             </div>
-            {imageClicked ?
-                <img 
-                    src={profPic} 
-                    alt="test-pictures"
-                    className="profilePicture"
-                /> :
-                <ActionButton
-                    onClick={()=> {setImageClicked(true)}}
-                 />    
-            }
         </div>
+        </div>
+        
     )
 }
